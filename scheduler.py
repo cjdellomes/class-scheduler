54e90536ec8598bc6a16520cd8e2653ac60e808a
Christopher Dellomes <cjdellomes@gmail.com>
2020-08-08 18:04:34 -0700

    Removed unnecessary Schedule class. Renamed ClassSession class to Session. Created Scheduler class. Initialized get_schedules static method.

--- scheduler.py
+++ scheduler.py
@@ -1,18 +1,12 @@
 import datetime
 
 
-class ClassSession:
+class Session:
 
     def __init__(self, day_of_week, start_time, end_time):
         self.day_of_week = day_of_week
         self.start_time = start_time
         self.end_time = end_time
-
-
-class Schedule:
-
-    def __init__(self, class_sessions):
-        self.class_sessions = class_sessions
 
 
 class Final:
@@ -32,11 +26,18 @@ class Professor:
 
 class Course:
 
-    def __init__(self, id, number, title, professor, units, schedule, final):
+    def __init__(self, id, number, title, professor, units, sessions, final):
         self.id = id
         self.number = number
         self.title = title
         self.professor = professor
         self.units = units
-        self.schedule = schedule
+        self.sessions = sessions
         self.final = final
+
+
+class Scheduler:
+
+    @staticmethod
+    def get_schdeules(courses, max_units, course_numbers):
+        pass
